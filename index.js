3e94dae5814cd4ff658f4d41d1a00c4fe62a1e4b
Brandon Castro <castroyou999@gmail.com>
2022-10-04 22:10:33 -0400

    commit tree 6c634b7c2fcd04cf051ee3a1076934eace8a2c57

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeRawData){
+    return {
+        firstName: employeeRawData[0],
+        familyName: employeeRawData[1],
+        title: employeeRawData[2],
+        payPerHour: employeeRawData[3],
+        timeInEvents: [],
+        timeOutEvents: [], 
+    }
+}
+function createEmployeeRecords(arrayOfEmployees){
+    return arrayOfEmployees.map(employee => createEmployeeRecord(employee))
+}
+function createTimeInEvent(dateStamp){
+    let [date,hour] = dateStamp.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour,10),
+        date,
+    })
+    return this
+}
+function createTimeOutEvent(dateStamp){
+    let [date,hour] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour,10),
+        date,
+    })
+    return this
+}
+function hoursWorkedOnDate(date){
+    let inEvent = this.timeInEvents.find((e)=> e.date === date);
+    let outEvent = this.timeOutEvents.find((e) => e.date === date);
+    return (outEvent.hour - inEvent.hour) /100
+}
+function wagesEarnedOnDate(date){
+    let rawWage = hoursWorkedOnDate.call(this, date) * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(e=> e.firstName === firstName)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -19,5 +62,13 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function calculatePayroll(employeeRecordsArray){
+    // const allEmployeeWages = employeeRecordsArray.map(employee => { return allWagesFor.call(employee)})
+    // return allEmployeeWages.reduce((total, wage) => {
+    //     return total + wage
+    // }, 0 )
+    return employeeRecordsArray.reduce( (total, employee) => {return total + allWagesFor.call(employee)},0)
 }
 
